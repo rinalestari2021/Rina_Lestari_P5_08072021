cb2a799e205f30754a60cfeaced29078ff201689
Rina Lestari <javagraphiste@outklook.comgit inittouch index.htmlgit add.git commit -mgit commit -m first>
2021-12-02 18:31:37 +0100

    fetch 021221

--- front/js/script.js
+++ front/js/script.js
@@ -4,60 +4,40 @@ let items = document.getElementById("items");
 console.log("Items : ", items);
 
 fetch("http://localhost:3000/api/products").then((response) => {
-  response.json().then((json) => {
-    for (let i = 0; i < data.lenght; i++) {
-      console.log(data);
+  response.json().then((products) => {
+    console.log("products :", products);
 
-      // create elements inside boucles & attribut
-      let section = document.querySelector(".items");
-      section.appendChild(link).setAttribute("href", "./product.html?id=42");
-      let items = document.createElement("article");
-      items.appendChild(article);
-      let link = document.createElement("a");
-      link.appendChild(article);
-      let img = document.createElement("img");
-      article.appendChild(img).setAttribute("src", data[i].imageUrl);
-      let h3 = document.createElement("h3");
-      article.appendChild(h3).setAttribute("class", "productName");
-      let p = document.createElement("p");
-      article.appendChild(p).setAttribute("class", "productDescription");
+    // create elements inside boucles & attribut
+    let items = document.querySelector(".items");
+    let article = document.createElement("article");
+    items.appendChild(article);
+    let link = document.createElement("a");
+    article.appendChild(link).setAttribute("href", "./product.html?id=42");
+    let img = document.createElement("img");
+    article.appendChild(img).setAttribute("src", data[i].imageUrl);
+    let h3 = document.createElement("h3");
+    article.appendChild(h3).setAttribute("class", "productName");
+    let p = document.createElement("p");
+    article.appendChild(p).setAttribute("class", "productDescription");
 
-      //adding tittle of attribut
-      document.querySelectorAll("h3")[i].innerHTML = data[i].name;
-      document.querySelectorAll("p")[i].innerHTML = data[i].description;
+    //adding tittle of attribut
+    document.querySelectorAll("h3")[i].innerHTML = data[i].name;
+    document.querySelectorAll("p")[i].innerHTML = data[i].description;
 
-      //parcourir le tableau products
-      // pour chaque product
-      // créer un élément <a>
-      // créer un élément <article>
-      // créer un élément <img>
-      // créer un élément <h3>
-      // créer un élément <p>
-      // mettre l'<article> dans le <a>
-      // mettre l'<img> dans le <article>
-      // mettre l'<h3> dans le <article>
-      // mettre l'<p> dans le <article>
-      // definir les attributs des éléments
-      // mettre le <a> dans items
-    }
+    //parcourir le tableau products
+    // pour chaque product
+    // créer un élément <a>
+    // créer un élément <article>
+    // créer un élément <img>
+    // créer un élément <h3>
+    // créer un élément <p>
+    // mettre l'<article> dans le <a>
+    // mettre l'<img> dans le <article>
+    // mettre l'<h3> dans le <article>
+    // mettre l'<p> dans le <article>
+    // definir les attributs des éléments
+    // mettre le <a> dans items
   });
 });
 
 //return fetch
-async function getArticles() {
-  return fetch("http://localhost:3000/api/products")
-    .then(function (response) {
-      return response.json();
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-}
-console.log(getArticles());
-
-function main() {
-  let articles = await getArticles();
-  for (i of articles) {
-    displayArticles(i);
-  }
-}
