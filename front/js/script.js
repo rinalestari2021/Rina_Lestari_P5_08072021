10d5d90ffef01cecb1290e2a5d2afb0926f7bdb6
Rina Lestari <javagraphiste@outklook.comgit inittouch index.htmlgit add.git commit -mgit commit -m first>
2021-12-06 07:53:32 +0100

    elements and attribut 061221

--- front/js/script.js
+++ front/js/script.js
@@ -4,30 +4,35 @@ let items = document.getElementById("items");
 console.log("items : ", items);
 
 fetch("http://localhost:3000/api/products").then((response) => {
-  response.json().then((products) => {
-    console.log("products :", products);
+  response.json().then((data) => {
+    console.log(data);
+    for (let items of data) {
+      items.innerHTML += "<img src=${products.url}>";
+    }
+  });
 
-    // create elements inside boucles & attribut
-    let items = document.querySelector("#items");
-    let article = document.createElement("article");
-    items.appendChild(article);
-    let link = document.createElement("a");
-    article.appendChild(link).setAttribute("href", "./product.html?id=42");
-    let img = document.createElement("img");
-    article.appendChild(img).setAttribute("src", "./images");
-    let h3 = document.createElement("h3");
-    article.appendChild(h3).setAttribute("class", "productName");
-    let p = document.createElement("p");
-    article.appendChild(p).setAttribute("class", "productDescription");
+  // create elements inside boucles & attribut
+  let items = document.querySelector("#items");
+  let article = document.createElement("article");
+  items.appendChild(article);
+  let link = document.createElement("a");
+  article.appendChild(link).setAttribute("href", "./product.html?id=42");
+  let img = document.createElement("img");
+  article.appendChild(img).setAttribute("src", "/back/images");
+  let h3 = document.createElement("h3");
+  article.appendChild(h3).setAttribute("class", "productName");
+  let p = document.createElement("p");
+  article.appendChild(p).setAttribute("class", "productDescription");
 
-    //adding tittle of attribut
-    document.querySelectorAll("a").innerHTML = products.href;
-    document.querySelectorAll("img").innerHTML = products.imageURL;
-    document.querySelectorAll("h3").innerHTML = products.name;
-    document.querySelectorAll("p").innerHTML = products.description;
+  //adding tittle of attribut
+  document.querySelectorAll("a").innerHTML = products.href;
+  document.querySelectorAll("img").innerHTML = products.imageURL;
+  document.querySelectorAll("h3").innerHTML = products.name;
+  document.querySelectorAll("p").innerHTML = products.description;
 
-    //tableau products
-    /*let products = [
+  //tableau products
+
+  /*let products = [
       "kanap01",
       "kanap02",
       "kanap03",
@@ -44,19 +49,25 @@ fetch("http://localhost:3000/api/products").then((response) => {
 
     products.forEach((item) => console.log(items));*/
 
-    //parcourir le tableau products
-    // pour chaque product
-    // créer un élément <a>
-    // créer un élément <article>
-    // créer un élément <img>
-    // créer un élément <h3>
-    // créer un élément <p>
-    // mettre l'<article> dans le <a>
-    // mettre l'<img> dans le <article>
-    // mettre l'<h3> dans le <article>
-    // mettre l'<p> dans le <article>
-    // definir les attributs des éléments
-    // mettre le <a> dans items
-    items.appendChild(a);
-  });
+  //parcourir le tableau products
+  // pour chaque product
+  // créer un élément <a>
+  // créer un élément <article>
+  // créer un élément <img>
+  // créer un élément <h3>
+  // créer un élément <p>
+  // mettre l'<article> dans le <a>
+  // mettre l'<img> dans le <article>
+  // mettre l'<h3> dans le <article>
+  // mettre l'<p> dans le <article>
+  // definir les attributs des éléments
+
+  // mettre le <a> dans items
+  let elt = document.getElementById("item");
+  elt.innerHTML = "<a href>";
 });
+
+function images() {
+  var img = document.getElementsByClassName(img);
+  img.style.visibility = visible ? "visible" : "hidden";
+}
