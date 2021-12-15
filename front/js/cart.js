6ce20711f57ccd717627a39230184b738cf35751
gurso <account+gitlab@urso.fr>
2021-12-15 18:29:08 +0100

    15/12/2021 session

---
+++ front/js/cart.js
@@ -0,0 +1,6 @@
+// chercher le contenu du panier dansle localStorage
+
+// envoyé la commande au serveur
+// url de l'api : http://localhost:3000/api/products/order
+
+// si la command est validé, rediriger l'utilisateur vers la page confirmation
--- front/js/product.js
+++ front/js/product.js
@@ -3,63 +3,66 @@ function fetchProducts(getId) {
   fetch(`http://localhost:3000/api/products/${getId}`)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
-      const imgNode = document.querySelector("div.item__img");
-      const img = document.createElement("img");
-      img.setAttribute("src", data.imageUrl);
-      img.setAttribute("alt", data.altTxt);
-      imgNode.appendChild(img);
-      document.getElementById(`title`).textContent = data.name;
-      document.getElementById(`price`).textContent = data.price;
-      document.getElementById(`description`).textContent = data.description;
+      console.log(data)
+      const imgNode = document.querySelector("div.item__img")
+      const img = document.createElement("img")
+      img.setAttribute("src", data.imageUrl)
+      img.setAttribute("alt", data.altTxt)
+      imgNode.appendChild(img)
+      document.getElementById(`title`).textContent = data.name
+      document.getElementById(`price`).textContent = data.price
+      document.getElementById(`description`).textContent = data.description
       //create color
-      const colors = document.getElementById("colors");
+      const colors = document.getElementById("colors")
       data.colors.forEach((color) => {
-        var option = document.createElement("option");
-        option.setAttribute("value", color);
-        option.innerText = color;
-        colors.appendChild(option);
-      });
-    });
+        var option = document.createElement("option")
+        option.setAttribute("value", color)
+        option.innerText = color
+        colors.appendChild(option)
+      })
+    })
 }
 function listen() {
   //select color
-  let colors = document.querySelector("#colors");
-  colors.addEventListener("click", {
-    capture: true,
-    once: true,
-    passive: true,
-  });
+  let colors = document.querySelector("#colors")
+  colors.addEventListener("change", function () {
+    console.log("change ON COLORS")
+    // je dois changer la couleur du canape
+  })
 
   //input quantity
-  var input = document.querySelector("#quantity");
-  var min = Number(input.getAttribute("min"));
-  var max = Number(input.getAttribute("max"));
+  // var input = document.querySelector("#quantity")
+  // var min = Number(input.getAttribute("min"))
+  // var max = Number(input.getAttribute("max"))
 
-  if (input !== undefined && input !== null) {
-    function quantity(e) {
-      var current = Number(input.value);
-      var newval = current - itemQuantity;
-      if (newval < min) {
-        newval = min;
-      } else if (newval > max) {
-        newval = max;
-      }
-      input.value = number(newval);
-      e.preventDefault();
-    }
-  }
+  // if (input !== undefined && input !== null) {
+  //   function quantity(e) {
+  //     var current = Number(input.value)
+  //     var newval = current - itemQuantity
+  //     if (newval < min) {
+  //       newval = min
+  //     } else if (newval > max) {
+  //       newval = max
+  //     }
+  //     input.value = number(newval)
+  //     e.preventDefault()
+  //   }
+  // }
 
   // event submit button
-  const addToCart = document.querySelector("input");
 
-  button.addEventListener("click", addToCart);
+  const button = null // use id to get button
+  button.addEventListener("click", function () {
+    console.log("la quantité est = ", input.value)
+    // ajouter au panier
+    // localStorage (voir sur mdn)
+  })
 }
 
 function main() {
-  var getId = new URLSearchParams(window.location.search).get("id");
-  fetchProducts(getId);
-  listen();
+  var getId = new URLSearchParams(window.location.search).get("id")
+  fetchProducts(getId)
+  listen()
 }
 
-main();
+main()
