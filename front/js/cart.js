6460e39eda270c6cacccf89ebd27047ed6accec9
Rina Lestari <javagraphiste@outklook.comgit inittouch index.htmlgit add.git commit -mgit commit -m first>
2021-12-28 22:18:36 +0100

    281221

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,88 +1,88 @@
 //get items from local storage
 function getCard() {
-  let check = localStorage.getItem("cart")
+  let check = localStorage.getItem("cart");
   if (check == null) {
-    return []
+    return [];
   } else {
-    return JSON.parse(check)
+    return JSON.parse(check);
   }
 }
-let cart = getCard()
+let cart = getCard();
 
 function saveCart(cart) {
-  localStorage.setItem("cart", JSON.stringify(cart))
+  localStorage.setItem("cart", JSON.stringify(cart));
 }
 
 // add product into cart
 function addCart(product) {
-  let foundProduct = this.cart.find((p) => p.id == product.id)
+  let foundProduct = this.cart.find((p) => p.id == product.id);
   if (foundProduct != undefined) {
-    foundProduct.quantityChoice++
+    foundProduct.quantityChoice++;
   } else {
-    product.quantity = 1
-    cart.push(product)
+    product.quantity = 1;
+    cart.push(product);
   }
-  saveCart(cart)
+  saveCart(cart);
 }
 
 function rmProduct() {
-  console.log("RM PRODUCT")
+  console.log("RM PRODUCT");
 }
 
 //remove items from cart ( isn't working yet)
 function removeFromCart(name, quantity = 0) {
   for (let i in cart) {
     if (cart[i].p.id === product.id) {
-      cart[i].quantity -= 1
+      cart[i].quantity -= 1;
       if (cart[i].quantity === 0) {
-        cart.splice(i, 1)
+        cart.splice(i, 1);
       }
       // showItem()
-      return
+      return;
     }
   }
-  cart = cart.filter((p) => p.id != product.id)
-  saveCart(cart)
+  cart = cart.filter((p) => p.id != product.id);
+  saveCart(cart);
 }
 
 //change quantity of items
 function changeQuantity(product, quantity) {
-  let foundProduct = this.cart.find((p) => p.id == product.id)
+  let foundProduct = cart.find((p) => p.id == product.id);
   if (foundProduct != undefined) {
-    foundProduct.quantity += quantity
+    foundProduct.quantity += quantity;
     if (foundProduct.quantity <= 0) {
-      remove(foundProduct)
+      remove(foundProduct);
     } else {
-      this.save()
+      cart.save();
     }
   }
 }
 
 //total of product
 function getNumberProduct(cart) {
-  let number = 0
+  let number = 0;
   for (let product of cart) {
-    number += product.quantity
+    number += product.quantity;
   }
-  return number
+  return number;
 }
 
 //total of price
 function settotalPrice(cart) {
-  const totalDiv = document.querySelector("#totalPrice")
-  let total = 0
+  const totalDiv = document.querySelector("#totalPrice");
+  let total = 0;
   for (let product of cart) {
-    total += product.quantity * product.price
+    total += product.quantity * product.price;
   }
-  totalDiv.innerText = total
+  totalDiv.innerText = total;
 }
 
 //to show the result of item selected
 function displayCart() {
-  let cartItems = document.getElementById("cart__items")
-  cartItems.innerHTML = ""
+  let cartItems = document.getElementById("cart__items");
+  cartItems.innerHTML = "";
   cart.forEach((product) => {
-    console.log(product)
+    console.log(product);
     cartItems.innerHTML += `
         <article class="cart__item" data-id=${product.id} data-color="${product.color}">
                 <div class="cart__item__img">
@@ -96,7 +96,7 @@ function displayCart() {
                   </div>
                   <div class="cart__item__content__settings">
                     <div class="cart__item__content__settings__quantity">
-                      <p>${product.quantity} : </p>
+                      <p>Qté :</p>
                       <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product.quantity}">
                     </div>
                     <div class="cart__item__content__settings__delete">
@@ -104,11 +104,12 @@ function displayCart() {
                     </div>
                   </div>
                 </div>
-        </article>`
-  })
+        </article>`;
+  });
 }
-displayCart(cart)
-settotalPrice(cart)
+displayCart(cart);
+settotalPrice(cart);
+getNumberProduct(cart);
 
 // gérer les problemes d'affichage // DONE // attention au ancienne données !
 
